604715de7c6a9af0a844d0e31dce58c467399838
Pedro <pedroignacioperezvargas@gmail.com>
2017-07-07 19:55:29 -0300

    It kinda works for now

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ AppDivs.amntUsed = [];
 var AppGraph = {};
 AppGraph.dom = document.getElementById("graph");
 AppGraph.g = AppGraph.dom.getContext('2d');
-AppGraph.pointSize = 1;
+AppGraph.pointSize = 3;
 //AppGraph.g.fillText("Bruh, like, bruh",30,30)
 AppGraph.size = {w:Number(AppGraph.dom.width),h:Number(AppGraph.dom.height)};
 
@@ -15,32 +15,45 @@ AppGraph.clear = function() {
 }
 
 AppGraph.draw = function(data,mode) {
-    if(typeof(data)!="object")
+    if(typeof(data)!="object") {
+        console.error("Bruh, dis is not an obj");
         return;
-    if(data.length==0)
+    }
+    if(data.length==0) {
+        console.error("Bruh, dis obj is emti");
         return;
+    }
+    var m = mode;
     if(typeof(data.x)=="number"&&typeof(data.y)=="number") {//Just one point
         AppGraph.point(data.x,data.y);
     } else {
+        //console.log("Dis is an arr of points");
         for(p of data) {
+            //console.log(p);
             if(typeof(p.x)=="number"&&typeof(p.y)=="number") {
-                if(mode=="point")
+                if(m=="point")
                     AppGraph.point(p.x,p.y);
-                if(mode=="line") {
-                    AppGraph.g.moveTo(p.x,p.y);
-                    mode = "line2";
-                }
-                if(mode=="line2") {
+                if(m=="line2") {
+                    //console.log("I'mma makin' da line at (%i;%i)",p.x,p.y);
                     AppGraph.g.lineTo(p.x,p.y);
+                }
+                if(m=="line") {
+                    AppGraph.g.moveTo(p.x,p.y);
+                    //console.log("I'mma startin' a line at (%i;%i)",p.x,p.y);
+                    m = "line2";
                 }
             }
         }
-        switch(mode) {
+        switch(m) {
             case "line2":
+            case "line":
+                AppGraph.g.fillStyle = "black";
                 AppGraph.g.stroke();
+                //console.log("Da line has ben draft");
                 break;
         }
     }
+    return 0;
 }
 AppGraph.point = function(x,y) {
     with(AppGraph) {
@@ -61,7 +74,7 @@ AppDivs.getDivs = function(n) {
     if(n==2)return 2;*/
     AppDivs.factorize(n);
     res = AppDivs.numsUsed.length;
-    console.log("%i has %i divs",n,res);
+    //console.log("%i has %i divs",n,res);
     return res;
 }
 
@@ -80,13 +93,13 @@ AppDivs.factorize = function(n) {//40
     for(var i=0;i<arrLength;i++) {
         var nNum = n/AppDivs.numsUsed[i];
         //console.log("%i / %i = %i",n,AppDivs.numsUsed[i],n);
-        if(AppDivs.numsUsed.lastIndexOf(nNum)==-1)
-            AppDivs.numsUsed.push(nNum);
+        /*if(AppDivs.numsUsed.lastIndexOf(nNum)==-1)
+            AppDivs.numsUsed.push(nNum);*/
     }
     //AppDivs.numsUsed.push(1);
     //AppDivs.numsUsed.push(n);
-    if(AppDivs.numsUsed.indexOf(n)!=AppDivs.numsUsed.lastIndexOf(n))
-        AppDivs.numsUsed.pop();
+    /*if(AppDivs.numsUsed.indexOf(n)!=AppDivs.numsUsed.lastIndexOf(n))
+        AppDivs.numsUsed.pop();*/
 }
 
 var domD = document.getElementById("d");
@@ -121,29 +134,26 @@ function process() {
     divs.min = min;
     for(var i=0;i<divs.length;i++) {
         divs[i].y = AppGraph.size.h-AppGraph.size.h/max*divs[i].d;
-        console.log("(%i;%i)",divs[i].x,divs[i].y);
+        //console.log("(%i;%i)",divs[i].x,divs[i].y);
     }
-    console.log("Biggest: %i\nSmallest: %i",max,min);
+    //console.log("Biggest: %i\nSmallest: %i",max,min);
     AppGraph.draw(divs,'point');
-    console.log("Range: %i > %i",min,max);
+    //console.log("Range: %i > %i",min,max);
+    var horz = [];
+    for(var d=0;d<max;d++) {
+        var y = AppGraph.size.h-AppGraph.size.h/max*d;
+        if(y%10==0) {
+            AppGraph.draw([{x:0,y:y},{x:AppGraph.size.w,y:y}],'line');
+        }
+    }
 }
 
 function h() {
+    if(divs.length==0)return;
     var rect = AppGraph.dom.getBoundingClientRect();
     var mouse = {x:event.clientX-rect.left,y:event.clientY-rect.top};
     i = Math.floor(mouse.x/(AppGraph.size.w/divs.rng));
-    console.log("N: %i has %i",Number(document.getElementById("ini").value)+i,divs[i].d);
+    //console.log("N: %i has %i",Number(document.getElementById("ini").value)+i,divs[i].d);
     domD.innerText = divs[i].d;
     domN.innerText = divs.ini+i;
-    /*
-    X = W/R*i
-    X = i*W/R
-    X/(W/R) = i
-    32 = 800/40*i
-    32 = i*(20/1)
-    32/20 = i
-    16/10 = i
-    1,6 = i
-    X/(W/R) = i
-    */
 }
