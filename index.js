51efa63b068d1f8364894f79e95542ecd5627079
Pedro <pedroignacioperezvargas@gmail.com>
2017-07-07 17:46:37 -0300

    waaaaaa it has a bug with 0, 1 and 2 D_:

--- index.js
+++ index.js
@@ -54,6 +54,8 @@ AppDivs.getDivs = function(n) {
     AppDivs.numsUsed = [];
     AppDivs.primes = [];
     if(n==0)return 0;
+    if(n==1)return 1;
+    if(n==2)return 2;
     /*if(n==1)return 1;
     if(n==2)return 2;*/
     AppDivs.factorize(n);
